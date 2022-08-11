b64640710f1c32763dde49a40e04a9c745a3b952
AKreft10 <arkadiuszkreft8@gmail.com>
2022-08-11 13:13:57 +0200

    Add new product feature, remove unnecessary return value

---
+++ ZooWebShopAPI/Feautures/Products/Handlers/AddNewProductHandler.cs
@@ -0,0 +1,39 @@
+﻿using MediatR;
+using ZooWebShopAPI.DataAccess;
+using ZooWebShopAPI.Entities;
+using ZooWebShopAPI.Feautures.Products.Commands;
+using ZooWebShopAPI.Models;
+
+namespace ZooWebShopAPI.Handlers;
+
+public class AddNewProductHandler : IRequestHandler<AddNewProductCommand>
+{
+    private readonly IDataAccess _dataAccess;
+
+    public AddNewProductHandler(IDataAccess dataAccess)
+    {
+        _dataAccess = dataAccess;
+    }
+
+
+    public async Task<Unit> Handle(AddNewProductCommand request, CancellationToken cancellationToken)
+    {
+        var productToAdd = new Product()
+        {
+            Name = request.dto.Name,
+            OriginalPrice = request.dto.OriginalPrice,
+            Price = request.dto.Price,
+            Photos = request.dto.Photos.Select(z => new Photo
+            {
+                PhotoUrl = z.PhotoUrl
+            }).ToList(),
+            ProductCategories = request.dto.Categories.Select(z => new ProductCategory()
+            {
+                CategoryId = z.CategoryId
+            }).ToList()
+        };
+
+        await _dataAccess.AddNewProduct(productToAdd);
+        return await Task.FromResult(Unit.Value);
+    }
+}

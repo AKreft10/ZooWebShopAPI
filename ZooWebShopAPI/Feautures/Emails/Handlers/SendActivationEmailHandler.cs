d3cd19c65d7bd187ebec3b6b987526de0acf53b0
AKreft10 <arkadiuszkreft8@gmail.com>
2022-08-15 20:44:03 +0200

    Email sending feature implemented Add a possibility to send an email for a newly created account

---
+++ ZooWebShopAPI/Feautures/Emails/Handlers/SendActivationEmailHandler.cs
@@ -0,0 +1,41 @@
+﻿using FluentEmail.Core;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ZooWebShopAPI.Feautures.Emails.Commands;
+
+namespace ZooWebShopAPI.Feautures.Emails.Handlers
+{
+    public class SendActivationEmailHandler : IRequestHandler<SendActivationEmailCommand>
+    {
+        private readonly IFluentEmailFactory _fluentEmail;
+
+        public SendActivationEmailHandler(IFluentEmailFactory fluentEmail)
+        {
+            _fluentEmail = fluentEmail;
+        }
+
+        public async Task<Unit> Handle(SendActivationEmailCommand request, CancellationToken cancellationToken)
+        {
+            var activateLink = "dasdsad";
+
+            var emailToSend = await _fluentEmail
+                .Create()
+                .To("xd.kenzi@gmail.com")
+                .Subject("test subject")
+                .UsingTemplateFromFile($"{Directory.GetCurrentDirectory()}/wwwroot/EmailTemplates/AccountActivationEmail.cshtml", new {activateLink})
+                .SendAsync();
+
+            Console.WriteLine(emailToSend.Successful);
+            var errors = emailToSend.ErrorMessages;
+
+            foreach (var item in errors)
+                Console.WriteLine(item);
+
+            return await Task.FromResult(Unit.Value);
+        }
+    }
+}
--- ZooWebShopAPI/Feautures/Products/Handlers/GetProductByIdHandler.cs
+++ ZooWebShopAPI/Feautures/Products/Handlers/GetProductByIdHandler.cs
@@ -7,6 +7,7 @@ using System.Threading.Tasks;
 using ZooWebShopAPI.DataAccess;
 using ZooWebShopAPI.Entities;
 using ZooWebShopAPI.Exceptions;
+using ZooWebShopAPI.Feautures.Emails.Commands;
 using ZooWebShopAPI.Feautures.Products.Queries;
 using ZooWebShopAPI.Models;
 
@@ -15,10 +16,12 @@ namespace ZooWebShopAPI.Handlers
     public class GetProductByIdHandler : IRequestHandler<GetProductByIdQuery, ProductModel>
     {
         private readonly IDataAccess _dataAccess;
+        private readonly IMediator _mediator;
 
-        public GetProductByIdHandler(IDataAccess dataAccess)
+        public GetProductByIdHandler(IDataAccess dataAccess, IMediator mediator)
         {
             _dataAccess = dataAccess;
+            _mediator = mediator;
         }
 
         public async Task<ProductModel> Handle(GetProductByIdQuery request, CancellationToken cancellationToken)
@@ -41,6 +44,8 @@ namespace ZooWebShopAPI.Handlers
                     PhotoUrl = z.PhotoUrl
                 }).ToList(),
             };
+
+            await _mediator.Send(new SendActivationEmailCommand());
 
             return resultProduct;
         }

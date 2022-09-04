77a106a5d55d9c32fa97a42a9348ca3beb0ce93c
AKreft10 <arkadiuszkreft8@gmail.com>
2022-09-04 23:42:33 +0200

    Implement a method to upload generated pdf invoice to the cloud and return an url address

---
+++ ZooWebShopAPI/Feautures/Invoices/Commands/UploadInvoiceCommand.cs
@@ -0,0 +1,11 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZooWebShopAPI.Feautures.Invoices.Commands
+{
+    public record UploadInvoiceCommand(byte[] invoice) : IRequest<string?>;
+}

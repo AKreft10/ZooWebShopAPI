a07b467238d874caefc076fc6aca1eb21f3b9af5
AKreft10 <arkadiuszkreft8@gmail.com>
2022-09-10 13:46:57 +0200

    Add validator for user reset password form

---
+++ ZooWebShopAPI/Validators/ResetPasswordDtoValidator.cs
@@ -0,0 +1,23 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ZooWebShopAPI.Dtos;
+
+namespace ZooWebShopAPI.Validators
+{
+    public class ResetPasswordDtoValidator : AbstractValidator<ResetPasswordDto>
+    {
+        public ResetPasswordDtoValidator()
+        {
+            RuleFor(z => z.Email)
+                .NotEmpty()
+                .EmailAddress();
+
+            RuleFor(z => z.ResetToken)
+                .NotNull();
+        }
+    }
+}
